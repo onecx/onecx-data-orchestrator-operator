fb207b526ec839235890067a7c79d521d6e120a0
Andrej Petras <andrej@lorislab.org>
2026-01-30 10:52:10 +0100

    fix: remove static init from configuration

--- src/main/java/org/tkit/onecx/data/orchestrator/operator/DataConfig.java
+++ src/main/java/org/tkit/onecx/data/orchestrator/operator/DataConfig.java
@@ -7,7 +7,6 @@ import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
-@StaticInitSafe
 @ConfigDocFilename("onecx-data-orchestrator-operator.adoc")
 @ConfigMapping(prefix = "onecx.data-orchestrator")
 @ConfigRoot(phase = ConfigPhase.RUN_TIME)
